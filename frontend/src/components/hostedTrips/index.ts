bbdde14f9d5900c5032a0bad972c50f150c4e5f4
Prateek Mohapatra <prateekmsoa@gmail.com>
2025-12-01 19:56:32 +0530

    req 8: remove host trip panel

--- frontend/src/components/hostedTrips/index.ts
+++ frontend/src/components/hostedTrips/index.ts
@@ -4,6 +4,5 @@
  */
 
 export { HostedTripsHeroSection } from "./HostedTripsHeroSection";
-export { HostTripPanel } from "./HostTripPanel";
 export { default as HostedTripCard } from "./HostedTripCard";
 export { JoinRequestsModal } from "./JoinRequestsModal";
